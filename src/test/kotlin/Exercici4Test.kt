418205a27a23ddd518169032e09fb76123db3798
Alvaro Ortega Marmol <info@codigonline.com>
2023-12-12 10:58:40 +0100

    Messages in Exercici4Test

--- src/test/kotlin/Exercici4Test.kt
+++ src/test/kotlin/Exercici4Test.kt
@@ -5,7 +5,9 @@ class Exercici4Test : GuiTest() {
     @Test
     fun `La funció fillArray especifica un valor aleatori per cada tarjeta`() {
         game.fillArray()
-        assert(game.cards.none { it == 0 })
+        assert(
+            game.cards.none { it == 0 },
+            { "Al executar la funció fillArray totes les posicions del array han de contenir dades" })
     }
 
     @Test
@@ -14,6 +16,9 @@ class Exercici4Test : GuiTest() {
         assert(game.cards.none { it == 0 })
         val map = game.cards.groupBy { it }.mapValues { (_, value) -> value.size }
         val allElementsAppearTwice = map.values.all { it == 2 }
-        assert(allElementsAppearTwice)
+        assert(
+            allElementsAppearTwice,
+            { "Al utilitzar la funció fillArray cada valor del array només pot estar repetit 2 vegades" }
+        )
     }
 }
